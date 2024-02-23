59b8f38b5a5c939d298bb5bbbd9e4508d505a4bc
ValDesign <valdesign.dev@gmail.com>
2024-02-23 10:30:48 +0100

    0.0.4

--- src/main/java/xyz/discordanalytics/utilities/ApiEndpoints.java
+++ src/main/java/xyz/discordanalytics/utilities/ApiEndpoints.java
@@ -1,7 +1,7 @@
 package xyz.discordanalytics.utilities;
 
 public class ApiEndpoints {
-    public static final String BASE_URL = "https://beta.discordanalytics.xyz/api";
+    public static final String BASE_URL = "https://discordanalytics.xyz/api";
     public static final String BOT_URL = "/bots/[id]";
     public static final String BOT_STATS = "/bots/[id]/stats";
 }
